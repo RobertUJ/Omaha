94826e0a8c63f2b4b12841c70bb769b50e80beec
erickhp12 <erickhp12@gmail.com>
2016-01-18 15:02:31 -0700

    se agregaron cambios

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from projects.views import IndexView
 
 urlpatterns = patterns(
     '',
-    url(  r'^$', IndexView.as_view(), name="IndexView"),
+    url(  r'^index/$', IndexView.as_view(), name="IndexView"),
 
 )
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from django.views.generic.base import View
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
 
+from projects.models import MainProject
 
 class IndexView(View):
     template_name = "index.html"
+
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
-
-
+        form = MainProject.objects.filter(project_name="Main project")
+        data = {
+            'project_name': form,
+        }
+        print form
+        return render(request,self.template_name, data )
