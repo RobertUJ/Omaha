a0c502c23785ab739999ef1062d519c584ef80e7
TrejoMF <mr24valves@icloud.com>
2016-01-17 23:23:47 -0700

    Se creo la primer vista que sera el login

--- omaha/settings.py
+++ omaha/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'omaha.users',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'omaha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__),'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- omaha/users/urls.py
+++ omaha/users/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.default   import patterns, url
+from django.conf.urls   import patterns, url
 
 urlpatterns = patterns('omaha.users.views',
                        url(r'^$','login_view',name='inicio_sesion'),
