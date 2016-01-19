cbdb2631f41851c5941d8bc17ca76791d0234463
TrejoMF <mr24valves@icloud.com>
2016-01-18 22:44:23 -0700

    se creo la app de login y se corrigieron los errores iniciales

--- login/views.py
+++ login/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
 
 # Create your views here.
+from django.template import RequestContext
+
+from login.forms import LoginForm
+
+
+def login_view(request):
+    form = LoginForm()
+    ctx = {'form':form}
+    return render_to_response('login.html',ctx,context_instance=RequestContext(request))
