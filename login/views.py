d24161f1d98bc987ef753f03ac655599069e59af
TrejoMF <mr24valves@icloud.com>
2016-01-21 19:13:56 -0700

    validacion de usuarios completo

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
-from django.http import request
+from django.http import request, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, redirect
 
 # Create your views here.
@@ -26,35 +26,35 @@ class LoginView(FormView):
             return render('inicio_de_sesion')
         return  render(request,self.template_name,ctx)
 
-    def form_valid(self, form):
-        mensaje = ""
-        username = form.cleaned_data["username"]
-        password = form.cleaned_data["password"]
-        print username
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request,user)
-                print username
-                #return redirect(reverse('index'))
-                mensaje = "Te has identificado de modo correcto"
-            else:
-                mensaje = "Tu usuario esta inactivo"
-                print mensaje
-        else:
-            mensaje = "Nombre y/o password incorrectos"
-            print mensaje
-        ctx = {'form':form,'mensaje':mensaje}
-        return super(LoginView, self).form_valid(form)
 
 
     def post(self, request, *args, **kwargs):
         #print request.POST
-
-
+        mensaje = ""
         form = LoginForm(request.POST)
-        ctx = {'form':form}
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            print request.POST
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    #login(request,user)
+                    #return redirect(reverse('projects:IndexView'))
+                    mensaje = "Te has identificado de modo correcto"
+                    print  mensaje
+                else:
+                    mensaje = "Tu usuario esta inactivo"
+                    print  mensaje
+            else:
+                mensaje = "Nombre y/o password incorrectos"
+                print  mensaje
+        #else:
+            #print "no, algo esta mal"
+
+        ctx = {'form':form,'mensaje':mensaje}
         return render(request,self.template_name,ctx)
+
 
 class LogoutView(TemplateView):
     def get(self, request, *args, **kwargs):
